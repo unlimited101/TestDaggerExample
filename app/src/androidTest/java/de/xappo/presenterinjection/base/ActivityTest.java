7702a48d646ddedcdfba74d7f4a4d522d477bf06
Michael Knoppik <knoppik@projektionisten.de>
2016-11-11 11:53:17 +0100

    ActivityTest put into right package

--- app/src/androidTest/java/de/xappo/presenterinjection/base/ActivityTest.java
+++ app/src/androidTest/java/de/xappo/presenterinjection/base/ActivityTest.java
@@ -1,7 +1,23 @@
 package de.xappo.presenterinjection.base;
 
+import org.junit.Before;
+
+import de.xappo.presenterinjection.di.components.TestActivityComponent;
+
 /**
  * Created by knoppik on 11.11.16.
  */
 public class ActivityTest {
+
+    private TestActivityComponent mTestActivityComponent;
+
+    @Before
+    public void setUp() {
+        initializeInjector();
+    }
+
+    protected void initializeInjector() {
+
+    }
+
 }
